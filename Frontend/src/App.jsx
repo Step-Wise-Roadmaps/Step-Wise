60f0c03da4e745f1014d8c7e07fbd4cd82376536
Neftalem <dneftalem@gmail.com>
2026-03-11 20:06:35 +0300

    Feat: create Navbar component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,10 @@
 
 function App() {
     return (
-        <div className="p-10 text-center bg-blue-500 text-white">
-            <h1 className="text-4xl font-bold">Tailwind</h1>
-        </div>
-    );
+        <>
+            
+        </>
+    )
 }
 
 export default App;
