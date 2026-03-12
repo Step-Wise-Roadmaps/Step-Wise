d422fa29d5fd29af33c9b22282f93ff43751525e
Neftalem <dneftalem@gmail.com>
2026-03-12 19:57:26 +0300

    feat: anmation and finishe Navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -14,12 +14,12 @@ function Navbar() {
                         <img className="w-32 sm:w-40 md:w-45 ml-4 sm:ml-6" src={MainLogo} alt="Main Logo" />
                     </div>
 
-                    <div className="hidden md:flex flex-none justify-center">
-                        <div className="flex space-x-10">
-                            <a href="#" class="hover:text-blue-400">Home</a>
-                            <a href="#" class="hover:text-blue-400">About</a>
-                            <a href="#" class="hover:text-blue-400">Contact</a>
-                        </div>
+                    <div className="hidden md:flex flex-none justify-center shadow-md shadow-cyan-950 rounded-full px-6 py-2">
+                        <nav className="flex space-x-10">
+                            <a href="#" className="hover:text-blue-400">Home</a>
+                            <a href="#" className="hover:text-blue-400">About</a>
+                            <a href="#" className="hover:text-blue-400">Contact</a>
+                        </nav>
                     </div>
 
                     
@@ -42,15 +42,19 @@ function Navbar() {
                     </div>
 
                 </div>
-                {isMenuOpen && (
-                    <div className="flex justify-center">
-                        <div className="w-150 items-center shadow-lg space-y-6 flex flex-col justify-center text-center border-t-1 border-blue-300 md:hidden p-4 space-y-2">
-                            <a href="#" class="hover:text-blue-400">Home</a>
-                            <a href="#" class="hover:text-blue-400">About</a>
-                            <a href="#" class="hover:text-blue-400">Contact</a>
+                {/* {isMenuOpen && ( */}
+                    <div className={`md:hidden overflow-hidden transition-all duration-500 ease-in-out ${isMenuOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"}`}>
+                        <div className="flex justify-center">
+                            <div className="w-150 items-center shadow-lg flex flex-col justify-center text-center border-t border-blue-300 p-4 space-y-4">
+                                <a href="#" className="hover:text-blue-400">Home</a>
+                                <a href="#" className="hover:text-blue-400">About</a>
+                                <a href="#" className="hover:text-blue-400">Contact</a>
+                                <button className="px-4 py-2 cursor-pointer hover:text-blue-400 duration-200">Sign in</button>
+                                <button className="w-full px-30 py-3 cursor-pointer bg-cyan-950 text-white rounded-[50px] hover:bg-cyan-800 duration-200">Sign up</button>
+                            </div>
                         </div>
                     </div>
-                )}
+                {/* )} */}
 
             </div>
         </>
