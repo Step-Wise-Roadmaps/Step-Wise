c868b2cd7c02a9fa79eff070f25bcd2050360a17
Neftalem <dneftalem@gmail.com>
2026-03-13 17:49:59 +0300

    Style: add google font and finishe change values

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -16,9 +16,9 @@ function Navbar() {
 
                     <div className="hidden md:flex flex-none justify-center shadow-md shadow-cyan-950 rounded-full px-10 py-2">
                         <nav className="flex space-x-10">
-                            <a href="#" className=" hover:text-cyan-800 duration-100">Method</a>
-                            <a href="#" className="hover:text-cyan-800 duration-100">Explore</a>
-                            <a href="#" className="hover:text-cyan-800 duration-100">FAQ</a>
+                            <a href="#" className="roboto-regular hover:text-cyan-800 duration-100">Method</a>
+                            <a href="#" className="roboto-regular hover:text-cyan-800 duration-100">Explore</a>
+                            <a href="#" className="roboto-regular hover:text-cyan-800 duration-100">FAQ</a>
                         </nav>
                     </div>
 
@@ -27,8 +27,8 @@ function Navbar() {
                     <div className="flex-1 flex justify-end mr-6">
 
                         <div className="hidden md:flex space-x-4">
-                            <button className="px-4 py-2 cursor-pointer hover:text-cyan-800 duration-200">Sign in</button>
-                            <button className="px-8 py-3 cursor-pointer bg-cyan-950 text-white rounded-[50px] hover:bg-cyan-800 duration-200">Sign up</button>
+                            <button className="roboto-regular px-4 py-2 cursor-pointer hover:text-cyan-800 duration-200">Sign in</button>
+                            <button className="roboto-regular px-8 py-[10px] cursor-pointer bg-cyan-950 text-white rounded-[50px] hover:bg-cyan-800 duration-200">Sign up</button>
                         </div>
                         <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="md:hidden focus:outline-none cursor-pointer">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-blue-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -46,11 +46,11 @@ function Navbar() {
                     <div className={`md:hidden overflow-hidden transition-all duration-500 ease-in-out ${isMenuOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"}`}>
                         <div className="flex justify-center">
                             <div className="w-150 items-center shadow-lg flex flex-col justify-center text-center border-t border-blue-300 p-4 space-y-4">
-                                <a href="#" className="hover:text-blue-400">Home</a>
-                                <a href="#" className="hover:text-blue-400">About</a>
-                                <a href="#" className="hover:text-blue-400">Contact</a>
-                                <button className="px-4 py-2 cursor-pointer hover:text-blue-400 duration-200">Sign in</button>
-                                <button className="w-full px-30 py-3 cursor-pointer bg-cyan-950 text-white rounded-[50px] hover:bg-cyan-800 duration-200">Sign up</button>
+                                <a href="#" className="roboto-regular hover:text-cyan-800 duration-100">Method</a>
+                                <a href="#" className="roboto-regular hover:text-cyan-800 duration-100">Explore</a>
+                                <a href="#" className="roboto-regular hover:text-cyan-800 duration-100">FAQ</a>
+                                <button className="roboto-regular px-4 py-2 cursor-pointer hover:text-cyan-800 duration-200">Sign in</button>
+                                <button className="roboto-regular w-full px-30 py-3 cursor-pointer bg-cyan-950 text-white rounded-[50px] hover:bg-cyan-800 duration-200">Sign up</button>
                             </div>
                         </div>
                     </div>
