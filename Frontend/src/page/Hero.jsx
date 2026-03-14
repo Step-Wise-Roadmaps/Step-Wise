63a43c323b272da776f822ef777632ca149041ea
Neftalem <dneftalem@gmail.com>
2026-03-14 12:55:42 +0300

    style: responsive values hero-section

--- Frontend/src/page/Hero.jsx
+++ Frontend/src/page/Hero.jsx
@@ -12,8 +12,13 @@ function Hero() {
 
                     <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white'>
                         <div>
-                            <h1 className='roboto-extrabold text-black text-8xl'>Any Skill,</h1>
-                            <h1 className='roboto-extrabold text-black text-7xl'>One Step at a Time.</h1>
+                            <h1 className='roboto-extrabold text-black text-4xl md:text-8xl xl:text-8xl [text-shadow:_2px_0px_5px_rgb(21_94_117)]'>Any Skill,</h1>
+                            <h1 className='roboto-extrabold text-black text-bace md:text-4xl xl:text-7xl [text-shadow:_2px_0px_5px_rgb(21_94_117)]'>One Step at a Time.</h1>
+                        </div>
+
+                        <div className='mt-1'>
+                            <p className='roboto-bold text-black text-xs md:text-2xl xl:text-3xl'>Smart Roadmaps for Fast Learning.</p>
+                            <p className='roboto-light text-black text-xs md:text-xl xl:text-2xl'>Skip the confusion of endless tutorials. Get a hand-picked, structured </p>
                         </div>
                     </div>
                 </div>
