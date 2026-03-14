0061074fe908299c811476116789d4bf69e60146
Neftalem <dneftalem@gmail.com>
2026-03-14 11:04:04 +0300

    Feat: add img

--- Frontend/src/page/Hero.jsx
+++ Frontend/src/page/Hero.jsx
@@ -1,8 +1,14 @@
 
+import heroImgg from '../assets/HeroImg/heroImgg.png'
+
 function Hero() {
     return(
         <>
-            
+            <div>
+                <div className='flex flex-col justify-center '>
+                    <img className='object-cover bg-no-repeat bg-center' src={heroImgg} alt="Hero Image" />
+                </div>
+            </div>
         </>
     )
 }
