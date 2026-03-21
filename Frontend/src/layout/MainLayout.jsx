e188988224f33b3b3910127ce4840436f7dfa15e
Neftalem <dneftalem@gmail.com>
2026-03-21 14:33:31 +0300

    Feature/footer section (#15)
    * feat: start dev FAQ
    
    * feat: done finished FAQ sections
    
    * feat: done add footer

--- Frontend/src/layout/MainLayout.jsx
+++ Frontend/src/layout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function MainLayout() {
 
     return(
         <>
             <Navbar variant="main"/>
-            <Outlet />
+                <Outlet />
+            <Footer />
         </>
     )
 }
