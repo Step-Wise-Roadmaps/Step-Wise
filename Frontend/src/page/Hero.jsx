226bf575e9b633dbd57bb459f3576e13818a76b3
Neftalem <dneftalem@gmail.com>
2026-03-23 21:54:54 +0300

    Feature/update hero section (#18)
    * feat: feat add animated video
    
    * Feat: finished animated

--- Frontend/src/page/Hero.jsx
+++ Frontend/src/page/Hero.jsx
@@ -7,6 +7,9 @@ import bookImg from '../assets/HeroImg/bookImg.png'
 import pointer from '../assets/HeroImg/pointer.png'
 import Photography from '../assets/HeroImg/Photography.png'
 
+// video
+import secondBackground from '../assets/video/secondBackground.mp4'
+
 const FAQItem = ({ question, answer }) => {
     const [isOpen, setIsOpen] = useState(false);
 
@@ -47,25 +50,41 @@ function Hero() {
             <div>
                 <div className='relative'>
                     <div className='flex flex-col justify-center'>
-                        <img className='object-cover saturate-200 contrast-75 bg-no-repeat bg-center mt-20 h-[89vh]' 
-                        src={heroImgg} 
-                        alt="Hero Image" />
-
+                        <video
+                        className='object-cover saturate-200 contrast-75 mt-20 h-[89vh] w-full'
+                        src={secondBackground}   // import your video like image
+                        autoPlay
+                        loop
+                        muted
+                        playsInline
+                        />
+                        
                         <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white'>
-                            <div>
-                                <h1 className='roboto-extrabold text-black text-4xl md:text-8xl xl:text-8xl [text-shadow:_2px_0px_5px_rgb(21_94_117)]'>Any Skill,</h1>
-                                <h1 className='roboto-extrabold text-black text-2xl md:text-4xl xl:text-7xl [text-shadow:_2px_0px_5px_rgb(21_94_117)]'>One Step at a Time.</h1>
-                            </div>
-
-                            <div className='mt-1'>
-                                <p className='roboto-bold text-black text-xs md:text-xl xl:text-3xl'>Smart Roadmaps for Fast Learning.</p>
-                                <p className='roboto-light text-black text-xs md:text-xl xl:text-2xl'>Skip the confusion of endless tutorials. Get a hand-picked, structured </p>
-                            </div>
-
-                            <div className='mt-10 md:mt-12'>
-                                <button className='px-6 py-4 md:px-14 md:py-4 md:text-lg focus:outline-hidden text-xs bg-cyan-950 rounded-full hover:bg-cyan-800 duration-200 cursor-pointer roboto-regular'>Start Learning</button>
-                            </div>
+                        <div>
+                            <h1 className='roboto-extrabold text-black text-4xl md:text-8xl xl:text-8xl [text-shadow:_2px_0px_5px_rgb(21_94_117)]'>
+                            Any Skill,
+                            </h1>
+                            <h1 className='roboto-extrabold text-black text-2xl md:text-4xl xl:text-7xl [text-shadow:_2px_0px_5px_rgb(21_94_117)]'>
+                            One Step at a Time.
+                            </h1>
                         </div>
+
+                        <div className='mt-1'>
+                            <p className='roboto-bold text-black text-xs md:text-xl xl:text-3xl'>
+                            Smart Roadmaps for Fast Learning.
+                            </p>
+                            <p className='roboto-light text-black text-xs md:text-xl xl:text-2xl'>
+                            Skip the confusion of endless tutorials. Get a hand-picked, structured
+                            </p>
+                        </div>
+
+                        <div className='mt-10 md:mt-12'>
+                            <button className='px-6 py-4 md:px-14 md:py-4 md:text-lg text-xs bg-cyan-950 rounded-full hover:bg-cyan-800 duration-200 cursor-pointer roboto-regular'>
+                            Start Learning
+                            </button>
+                        </div>
+                        </div>
+
                     </div>
                 </div>
 
