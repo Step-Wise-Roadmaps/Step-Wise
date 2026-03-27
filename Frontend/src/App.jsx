a5672cea30afd56082682f77423368468c6d6c01
Neftalem <dneftalem@gmail.com>
2026-03-27 19:43:04 +0300

    feat: creat new file starte dev Login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from "./layout/MainLayout";
 
 // pages
 import Hero from "./page/Hero";
+import Login from "./page/Login";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<MainLayout />}>
                     <Route index element={<Hero />} />
                 </Route>
+                <Route path="login" element={<Login/>} />
             </Routes>
         </>
     )
