1682ded6bf4b3f4f938fbf6198a2f1f9ea2f4725
Neftalem <dneftalem@gmail.com>
2026-04-13 18:05:57 +0300

    fix: resolve responsive issue

--- Frontend/src/page/AdminDashboard.jsx
+++ Frontend/src/page/AdminDashboard.jsx
@@ -7,42 +7,42 @@ import profilePhoto from "../assets/sideBarLogo/profilePhoto.png"
 function AdminDashboard() {
     return (
         <>
-            <div className="flex bg-[#F9FAFB] w-full h-screen space-x-20 md:space-x-12">
+            <div className="flex bg-[#F9FAFB] w-full h-screen md:space-x-20 md:space-x-12">
                 <div>
                     <AdminSideBar />
                 </div>
-                <div className="flex flex-col w-full m-6 space-y-15">
+                <div className="flex flex-col w-full m-6 space-y-10">
                     <div className="flex items-center ">
-                        <h1 className="roboto-bold text-2xl text-cyan-900">Welcom To Admin Dashboard</h1>
+                        <h1 className="roboto-bold text-lg md:text-2xl ml-15 md:ml-0 text-cyan-900">Welcom To Admin Dashboard</h1>
                     </div>
 
-                    <div className="p-6 bg-white rounded-xl flex-col space-y-10 md:flex md:space-x-30">
+                    <div className="p-6 bg-white rounded-xl flex flex-col gap-6 sm:grid sm:grid-cols-2 lg:grid-cols-4">
                         <div className=" flex items-center space-x-4">
-                            <img className="w-20 h-20 rounded-[50px]" src={profilePhoto} alt="" />
+                            <img className="w-16 h-16 md:w-20 md:h-20 rounded-full" src={profilePhoto} alt="" />
                             <div>
                                 <p className="roboto-regular text-md">Students</p>
                                 <h1 className="roboto-semibold text-2xl">200</h1>
                             </div>
                         </div>
 
-                        <div className="flex items-center space-x-4">
-                            <img className="w-20 h-20 rounded-[50px]" src={profilePhoto} alt="" />
+                        <div className=" flex items-center space-x-4">
+                            <img className="w-16 h-16 md:w-20 md:h-20 rounded-full" src={profilePhoto} alt="" />
                             <div>
                                 <p className="roboto-regular text-md">Students</p>
                                 <h1 className="roboto-semibold text-2xl">200</h1>
                             </div>
                         </div>
 
-                        <div className="flex items-center space-x-4">
-                            <img className="w-20 h-20 rounded-[50px]" src={profilePhoto} alt="" />
+                        <div className=" flex items-center space-x-4">
+                            <img className="w-16 h-16 md:w-20 md:h-20 rounded-full" src={profilePhoto} alt="" />
                             <div>
                                 <p className="roboto-regular text-md">Students</p>
                                 <h1 className="roboto-semibold text-2xl">200</h1>
                             </div>
                         </div>
 
-                        <div className="flex items-center space-x-4">
-                            <img className="w-20 h-20 rounded-[50px]" src={profilePhoto} alt="" />
+                        <div className=" flex items-center space-x-4">
+                            <img className="w-16 h-16 md:w-20 md:h-20 rounded-full" src={profilePhoto} alt="" />
                             <div>
                                 <p className="roboto-regular text-md">Students</p>
                                 <h1 className="roboto-semibold text-2xl">200</h1>
