9936859852680477951f36f93f349fcd7959e6ac
Neftalem <dneftalem@gmail.com>
2026-04-14 10:26:56 +0300

    feat: feat countUser logic

--- Backend/controller/adminController.js
+++ Backend/controller/adminController.js
@@ -3,7 +3,17 @@ const pool = require('../config/db');
 exports.getAllUsers = async (req, res) => {
     try {
         const [users] = await pool.query("SELECT id, full_name, email, role, created_at FROM users");
-        res.status(200).json(users);
+
+        const [countResult] = await pool.query(
+            "SELECT COUNT(*) AS total FORM users"
+        )
+        const totalUsers = countResult[0].total;
+
+        res.status(200).json({
+            success: true,
+            count: totalUsers,
+            data: users
+        });
     } catch (err) {
         res.status(500).json({ message: "Database Error", error: err.message });
     }
