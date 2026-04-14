0cdc147cbf3d85c2c88a46d9a51eba10591cd1f5
Neftalem <dneftalem@gmail.com>
2026-04-14 10:33:02 +0300

    feat: getAllUsers data from backend to adminService

--- Frontend/src/services/adminDashboardService.js
+++ Frontend/src/services/adminDashboardService.js
@@ -1,2 +1,13 @@
 import axiosInstance from "../api/axiosInstance";
 
+const getAllUsers = async (adminData) => {
+    const response = await axiosInstance.get('/admin/getAllUsers', adminData);
+
+    return response.data;
+}
+
+const adminDashboardService = {
+    getAllUsers,
+}
+
+export default adminDashboardService;
