c9c01d36af2e8b7329897b0ed3982f61ed089fc5
Neftalem <dneftalem@gmail.com>
2026-05-05 20:04:12 +0300

    style: style SearchBar

--- Frontend/src/components/UserManagement/SearchBar.jsx
+++ Frontend/src/components/UserManagement/SearchBar.jsx
@@ -2,10 +2,10 @@ import { UserSearch } from "lucide-react";
 
 function SearchBar() {
   return (
-    <div className='flex justify-end'>
-      <div className='relative'>
-        <input type="text" placeholder='Search' className='p-2 border rounded-xl' />
-        <UserSearch className="absolute right-3 top-2" />
+    <div className='flex items-center justify-end'>
+      <div className='relative flex items-center'>
+          <input type="text" placeholder='Search' className='p-2 md:px-5 border border-slate-400 rounded-xl shadow-md outline-hidden' />
+          <label className='absolute right-3 cursor-pointer text-slate-400'><UserSearch size={20} /></label>
       </div>
     </div>
   );
