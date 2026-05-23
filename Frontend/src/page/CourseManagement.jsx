6ff872767f55b99ea1d65e36ddf0da881916fc08
Neftalem <dneftalem@gmail.com>
2026-05-23 10:47:56 +0300

    Feat: add CourseHeader components

--- Frontend/src/page/CourseManagement.jsx
+++ Frontend/src/page/CourseManagement.jsx
@@ -1,6 +1,11 @@
+import CourseHeader from "../components/CourseManagement/CourseHeader";
 
 function CourseManagement() {
-
+    return(
+        <>
+            <CourseHeader />
+        </>
+    )
 }
 
 export default CourseManagement;
