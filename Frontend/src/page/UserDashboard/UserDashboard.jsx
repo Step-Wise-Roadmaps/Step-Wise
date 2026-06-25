9a4ae006501f545f0c8cbcf3e44685440c6fc113
Neftalem <dneftalem@gmail.com>
2026-06-25 10:03:49 +0300

    feat: add dashboard header

--- Frontend/src/page/UserDashboard/UserDashboard.jsx
+++ Frontend/src/page/UserDashboard/UserDashboard.jsx
@@ -1,10 +1,26 @@
 
+import { BookOpen } from "lucide-react";
+import sideBarLogo from "../../assets/sideBarLogo/sideBarLogo.png";
+
 function UserDashboard() {
 
     return (
         <>
-            <section className="relative overflow-hidden rounded-[2rem] border border-slate-200 bg-white p-6 md:p-8 shadow-[0_24px_80px_rgba(15,23,42,0.08)]">
-                
+            <section className="relative overflow-hidden rounded-xl border border-slate-200 bg-white p-6 md:p-10 shadow-[0_24px_80px_rgba(15,23,42,0.08)]">
+                <div className="absolute inset-0 bg-[radial-gradient(circle_at_top_right,_rgba(34,211,238,0.16),_transparent_30%),radial-gradient(circle_at_bottom_left,_rgba(59,130,246,0.10),_transparent_28%)]" />
+
+                <div className="relative flex justify-between">
+                    <div className="max-w-2xl space-y-2">
+                        <h1 className="roboto-semibold text-3xl">Welcome Neftalem! 👋</h1>
+                        <div className="flex gap-2">
+                            <span className="text-slate-300"><BookOpen /></span>
+                            <p className="roboto-extralight text-slate-500">Start Learning web development </p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src={sideBarLogo} alt="" />
+                    </div>
+                </div>
             </section>
         </>
     );
