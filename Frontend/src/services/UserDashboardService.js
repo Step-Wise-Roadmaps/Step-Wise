47a5754253e3897656f52358aa63bfca6ac84f14
Neftalem <dneftalem@gmail.com>
2026-06-25 16:18:04 +0300

    feat: create UserDashboardService

---
+++ Frontend/src/services/UserDashboardService.js
@@ -0,0 +1,26 @@
+
+import axiosInstance from '../api/axiosInstance';
+
+const getCourses = async (coursesData) => {
+    const response = await axiosInstance.get('/user/course', {
+        params: coursesData
+    })
+
+    return response.data.data
+}
+
+const getLessons = async (lessonData) => {
+    const response = await axiosInstance.get('/user/getLesson', {
+        params: lessonData
+    });
+
+    return response.data.data
+}
+
+const UserDashboardService = {
+    getLessons,
+    getLessons,
+
+}
+
+export default UserDashboardService;
