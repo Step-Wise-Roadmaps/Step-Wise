2f9598f67309788d1e7c0f0222e09b87917e925d
Neftalem <dneftalem@gmail.com>
2026-07-01 15:48:08 +0300

    feat: add lesson sideBar responsive layout

---
+++ Frontend/src/components/UserDashboardCommponents/UserSideBarManagment/LessonSideBarContent.jsx
@@ -0,0 +1,6 @@
+
+function LessonSideBarContent() {
+    return
+}
+
+export default LessonSideBarContent;
--- Frontend/src/page/UserDashboard/LessonSideBar.jsx
+++ Frontend/src/page/UserDashboard/LessonSideBar.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { Menu, X, Circle, CheckCircle } from "lucide-react";
+import { PanelLeftOpen, PanelLeftClose } from "lucide-react";
 import { getCoursesLessonsByCourcesId } from "../../features/auth/authSlice";
+
+import LessonSideBarContent from "../../components/UserDashboardCommponents/UserSideBarManagment/LessonSideBarContent";
 
 function LessonSideBar() {
   const { id } = useParams();
@@ -52,7 +54,40 @@ function LessonSideBar() {
 
   return (
     <>
-        
+        <button 
+          type="button"
+          onClick={() => setIsMobileOpen(true)}
+          className="fixed left-4 top-4 cursor-pointer w-11 h-11 flex items-center justify-center rounded-xl border border-slate-200 bg-white text-slate-700 shadow-lg md:hidden">
+            <PanelLeftOpen size={22}/>
+        </button>
+
+        <div className="hidden md:block">
+          <LessonSideBarContent 
+            lessonsWithCoursesId={lessonsWithCoursesId}
+            isLoading={isLoading}
+            />
+        </div>
+
+        <div className={`md:hidden fixed inset-0 z-50 ${isMobileOpen ? "pointer-events-auto backdrop-blur-sm" : "pointer-events-none"}`}>
+
+          <div className={`h-full max-w-[86vw] transition-transform duration-300 ease-out ${
+            isMobileOpen ? "translate-x-0" : "-translate-x-full"
+            }`}>
+              <LessonSideBarContent 
+                lessonsWithCoursesId={lessonsWithCoursesId}
+                isLoading={isLoading}
+                onMobileClose={() => setIsMobileOpen(false)}
+                mobile
+            />
+            </div>
+
+            <button
+              type="button"
+              onClick={() => setIsMobileOpen(false)}
+              className="absolute inset-0 -z-10"
+            />
+          
+        </div>
     </>
   );
 }
