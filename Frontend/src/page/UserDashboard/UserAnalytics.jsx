7c9cc9cde0b0e7825a9f41ab7c4a9ddff00b2294
Neftalem <dneftalem@gmail.com>
2026-07-06 13:15:48 +0300

    feat: import recharts

--- Frontend/src/page/UserDashboard/UserAnalytics.jsx
+++ Frontend/src/page/UserDashboard/UserAnalytics.jsx
@@ -1,6 +1,15 @@
+'use client';
+
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 
 function UserAnalytics() {
-    return(<></>)
+    return(
+        <>
+             <div className="w-full bg-white min-h-[550px] flex items-center rounded-2xl p-5 md:p-10">
+                
+             </div>
+        </>
+    )
 }
 
-export default UserAnalytics
+export default UserAnalytics;
